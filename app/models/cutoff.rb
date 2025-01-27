ca1dc11a2c05b676ed2335f959c87060a3a96e66
bottles10 <henryaovare@gmail.com>
2025-01-27 05:43:40 +0000

    Add current school scope to cutoff model

--- app/models/cutoff.rb
+++ app/models/cutoff.rb
@@ -1,6 +1,6 @@
 class Cutoff < ApplicationRecord
   belongs_to :school, default: -> { Current.school }
-  validates :current_basic, presence: true, uniqueness: true
+  validates :current_basic, presence: true, uniqueness: {scope: [:school_id] }
   validates :class_percentage, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :exam_percentage, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 
