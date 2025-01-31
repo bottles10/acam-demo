4283f7c08076c775a8ad2def271043c0ab16f52e
bottles10 <henryaovare@gmail.com>
2025-01-31 21:19:17 +0000

    Commentout modern browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   before_action :set_current_school
   before_action :configure_permitted_parameters, if: :devise_controller?
 
