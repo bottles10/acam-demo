978faaf66206408fd1571f5afe6eb38f6bdd3bec
bottles10 <henryaovare@gmail.com>
2025-07-09 23:19:21 -0500

    Add validation to check sum of cutoff percentage

--- app/models/cutoff.rb
+++ app/models/cutoff.rb
@@ -5,4 +5,15 @@ class Cutoff < ApplicationRecord
   validates :exam_percentage, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :total_subject, presence: true, numericality: { greater_than: 0 }
 
+	validate :check_sum_of_class_and_exam_cutoff
+
+
+  private
+
+  def check_sum_of_class_and_exam_cutoff
+		if class_percentage.to_f + exam_percentage.to_f != 100
+      errors.add(:base, "Class and Exam percentages must sum to 100%")
+    end
+  end
+
 end
